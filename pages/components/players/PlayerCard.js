f40d32298c001c52ed9bc2204821fd24147383d2
Danica Velarde <danica.cristine.velarde@gmail.com>
2022-03-25 01:51:11 -0400

    initial work on the players page and individial player card

--- pages/components/players/PlayerCard.js
+++ pages/components/players/PlayerCard.js
@@ -1,8 +1,25 @@
+import styles from './PlayerCard.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChartColumn, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+
 export default function PlayerCard(props) {
+  console.log(props.positions);
+  const positionsLabel = props.positions.join(' / ');
   return (
-    <div>
-      <h2>{props.firstName}</h2>
-      <h2>{props.lastName}</h2>
+    <div className={styles.playerCard}>
+      <div>
+        <h2>{props.firstName}</h2>
+        <h2>{props.lastName}</h2>
+      </div>
+      <FontAwesomeIcon
+          className={styles.playerMenuIcon}
+          icon={faEllipsisVertical}
+        />
+      <div><span className={styles.positionsText}>{positionsLabel}</span></div>
+      <FontAwesomeIcon
+          className={styles.statsIcon}
+          icon={faChartColumn}
+        />
     </div>
   );
 }
--- pages/components/players/index.js
+++ pages/components/players/index.js
@@ -1,11 +1,14 @@
 import styles from "./Players.module.css";
 import PlayerCard from "./PlayerCard";
+import PlayersFilter from "./PlayersFilter";
 
 export default function Players(props) {
   return (
-    <div>
-      <h1>Players</h1>
+    <div className={styles.playersContainer}>
+      <h1 className={styles.playersTitle}>Players</h1>
+      <PlayersFilter />
       <PlayerCard firstName="Tobio" lastName="Kageyama" positions={["S"]} />
+      <PlayerCard firstName="Daichi" lastName="Sawamura" positions={["C", "WS", "OH"]} />
     </div>
   );
 }
