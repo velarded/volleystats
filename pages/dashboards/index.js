8e5764aad47b1d1a351cf640335d85aa51289fc8
Danica Velarde <danica.cristine.velarde@gmail.com>
2022-04-17 12:37:39 -0400

    initial commit of dashboard and settings page

---
+++ pages/dashboards/index.js
@@ -0,0 +1,9 @@
+import MainContainer from "../components/mainContainer";
+
+export default function Dashboards(props) {
+  return (
+    <MainContainer>
+      <div>Dashboards Page</div>
+    </MainContainer>
+  );
+}
---
+++ pages/settings/index.js
@@ -0,0 +1,9 @@
+import MainContainer from "../components/mainContainer";
+
+export default function Settings(props) {
+  return (
+    <MainContainer>
+      <div>Settings Page</div>
+    </MainContainer>
+  );
+}
