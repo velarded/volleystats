263e6c85e1efd2615d9cd1fd79ef82cada58ac73
Danica Velarde <danica.cristine.velarde@gmail.com>
2023-02-04 16:38:15 -0500

    initial work to hero and features container on home page

--- pages/components/heroContainer/index.js
+++ pages/components/heroContainer/index.js
@@ -1,5 +1,11 @@
 import styles from "./HeroContainer.module.css";
 
 export default function HeroContainer(props) {
-  return <div className={styles.heroContainer}>Hero Container</div>;
+  return ( 
+  <div className={`h-screen w-full ${styles.heroContainer}`}>
+    <h1 className={`text-5xl uppercase tracking-widest p-20 ${styles.heroTitle}`}>Volleystats</h1>
+    <p className={`${styles.heroParagraph}`}>Record your volleyball statistics and visualize your in-game performances
+      to provide insight on areas to improve</p>
+  </div>
+  );
 }
